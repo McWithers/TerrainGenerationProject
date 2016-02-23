02800e71bfda175de7d92d49a6632f5adf8013a6
McWithers <withersc@purdue.edu>
2016-02-23 00:13:44 -0500

    Added a new file for Isometric Rendering

---
+++ Isometric_Render.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Isometric_Render {
+private:
+	int x_l;
+	int x_u;
+	int y_l;
+	int y_u;
+	int z_l;
+	int z_u;
+public:
+	void terrain_generation();
+	int * get_x();
+	int * get_y();
+	int * get_z();
+	void set_x(int, int);
+	void set_y(int, int);
+	void set_z(int, int);
+};
+
+int * Isometric_Render::get_x() {
+	int result[2];
+	result[0] = this->x_l;
+	result[1] = this->x_u;
+	return result;
+}
+
+int * Isometric_Render::get_y() {
+	int result[2];
+	result[0] = this->y_l;
+	result[1] = this->y_u;
+	return result;
+}
+
+int * Isometric_Render::get_z() {
+	int result[2];
+	result[0] = this->z_l;
+	result[1] = this->z_u;
+	return result;
+}
+
+void Isometric_Render::set_x(int x_l, int x_u) {
+	this->x_l = x_l;
+	this->x_u = x_u;
+}
+
+void Isometric_Render::set_y(int y_l, int y_u) {
+	this->y_l = y_l;
+	this->y_u = y_u;
+}
+
+void Isometric_Render::set_z(int z_l, int z_u) {
+	this->z_l = z_l;
+	this->z_u = z_u;
+}
+
+void Isometric_Render::terrain_generation() {
+
+}
