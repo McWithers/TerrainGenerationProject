26f9c616506091a38afbb295dff61a4b3dd20b61
McWithers <withersc@purdue.edu>
2016-02-23 01:00:42 -0500

    Updated main with for loop to eventually plot functions

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,17 @@
 
 int main() {
 	World_Map Plot(50,50,50);
-	
+	int i;
+	int j;
+	int k;
+	for (i = 0; i < Plot.get_x(); i++) {
+		for (j = 0; j < Plot.get_y(); j++) {
+			for (k = 0; k < Plot.get_z(); k++) {
+				(int) (25 + 10 * sin(i));
+				
+			}
+		}
+	}
 
 
 
